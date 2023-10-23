63b2cfae9e36e20196cdac9b680353571183a031
Michael Hum <michaelhum28@gmail.com>
2023-10-23 18:19:15 -0400

    feat layout

--- pages/pokemon.vue
+++ pages/pokemon.vue
@@ -1,8 +1,82 @@
 <template>
-    <div>
-        <string>Pokemans = bulbasaur</string>
-        Pokemans
-        <!-- yo idk how to link back to the index page wihtout writing this link -->
-        <p></p><a href="http://localhost:3000" class="text-white bg-green-700 hover:bg-green-800 focus:ri  ng-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">I dont want to see pokemans no mo</a>
+    <div class  = "container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
+        <h1 class ="subheading grey--text text-4xl">Pokemon Name</h1>
+        <div class="flex flex-row">
+  <div class="basis-1 text-3xl">Contents</div>
+  </div>
+ 
+</div>  
+<div class="text-xl">Bulbasaur is a Grass/Poison type Pokémon introduced in Generation 1.<p></p>
+
+Bulbasaur is a small, mainly turquoise amphibian Pokémon with red eyes and a green bulb on its back. It is based on a frog/toad, with the bulb resembling a plant bulb that grows into a flower as it evolves.
+
+Bulbasaur is notable for being the very first Pokémon in the National Pokédex. It is one of the three choices for a starter Pokémon in the original Game Boy games, Pokémon Red & Blue (Red & Green in Japan), along with Charmander and Squirtle.</div>
+
+
+
+<div class="container py-10 px-10 mx-0 min-w-full flex flex-col relative h-100 w-100">
+    <div class="flex mb-4">
+
+
+
+<div class="w-1/3 h-12 text-xl flex flex-col gap-y-1.5">
+    <div class="text-xl flex flex-col gap-y-56">
+    <img class="w-1/3 h-auto max-w-xs" src="https://img.pokemondb.net/artwork/large/bulbasaur.jpg" alt="image description">
+    
+    <div class="font-bold text-2xl">Base Stats</div>
+</div>
+
+    <div class="basis-1/4">HP</div>
+    <div class="basis-1/4">Attack</div>
+    <div class="basis-1/2">Defense</div>
+    <div class="basis-1/2">Sp. Atk</div>
+    <div class="basis-1/2">Sp. Def</div>
+    <div class="basis-1/2">Speed</div>
+    <div class="basis-1/2">Total</div>
+    <div class="w-1/3 h-12 text-xl flex flex-col gap-y-36">
+
     </div>
+</div>
+<!-- Spacing -->
+
+
+<div class="w-1/3 h-12 text-xl flex flex-col gap-y-1">
+    <div class="font-bold text-2xl">Pokedex Stats</div>
+    <div class="basis-1/4">National №</div>
+    <div class="basis-1/4">Type</div>
+    <div class="basis-1/2">Height</div>
+    <div class="basis-1/2">Weight</div>
+    <div class="basis-1/2">Abilities</div>
+    <div class="basis-1/2">Local №</div>
+</div>
+
+<div class="text-xl flex flex-col gap-y-60">
+<div class="w-100 h-12 text-xl flex flex-col gap-y-1">
+    <div class="font-bold text-2xl">Training</div>
+    <div class="basis-1/4">Ev Yield</div>
+    <div class="basis-1/4">Catch Rate</div>
+    <div class="basis-1/4">Base Friendship</div>
+    <div class="basis-1/2">Base Exp.</div>
+    <div class="basis-1/2">Growth Rate</div>        
+</div>
+
+<div class="text-xl flex flex-col gap-y-56">
+<div class="w-100 h-12 text-xl flex flex-col gap-y-1">
+    <div class="font-bold text-2xl">Breeding</div>
+    <div class="basis-1/4">Egg Groups</div>
+    <div class="basis-1/4">Gender</div>
+    <div class="basis-1/4">Egg Cycles</div>      
+</div>
+<div class="w-100 h-12 text-xl flex flex-col gap-y-1">
+    <div class="font-bold text-2xl">Type Defenses</div>    
+    </div>
+
+</div>
+
+</div>
+
+</div>
+</div>
+
+
 </template>
