e3f30381c6974c4804fc4b5165e9e8b4fb10e06c
Reuben D'Souza <reubenjds@gmail.com>
2023-10-23 12:38:31 -0400

    chore: change a tag to NuxtLink

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
         <p>Home page</p>
-        <a href="pokemon" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Read more</a>
+        <NuxtLink to="/pokemon" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            Goto pokemon
+        </NuxtLink>
 
     </div>
 </template>
