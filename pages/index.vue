71e9f4501a2e9346840667c7f88bd0689062dc49
Reuben D'Souza <reubenjds@gmail.com>
2023-10-23 12:23:08 -0400

    feat: add daisyui

--- pages/index.vue
+++ pages/index.vue
@@ -4,5 +4,5 @@
         <a href="pokemon" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Read more</a>
 
     </div>
-  </template>
+</template>
   
