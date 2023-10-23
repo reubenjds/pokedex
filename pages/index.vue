f29be21bb196cf87cfd5f695e8fe237af54e0dcc
Michael Hum <michaelhum28@gmail.com>
2023-10-23 02:03:18 -0400

    feat buttons and text

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        Home page
+    <div class="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
+        <p>Home page</p>
+        <a href="pokemon" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Read more</a>
+
     </div>
   </template>
   
--- pages/pokemon.vue
+++ pages/pokemon.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
-        Pokemon test
+        <string>Pokemans = bulbasaur</string>
+        Pokemans
+        <!-- yo idk how to link back to the index page wihtout writing this link -->
+        <p></p><a href="http://localhost:3000" class="text-white bg-green-700 hover:bg-green-800 focus:ri  ng-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">I dont want to see pokemans no mo</a>
     </div>
 </template>
