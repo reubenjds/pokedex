954872164f8547428d6ca6628f0dc9102f704a1a
Reuben D'Souza <reubenjds@gmail.com>
2023-11-11 18:44:37 -0500

    feat: redesign pokemon page

--- pages/pokemon.vue
+++ pages/pokemon.vue
@@ -1,155 +1,75 @@
+<!-- @format -->
+
 <template>
-  <div class="flex items-center justify-center pt-6 text-3xl">Bulbasaur</div>
-  
-  <div class="flex mb-4">
-    <!-- img -->
-    <div class="w-1/3 h-12">
-      <img class ="ps-20 pt-6 h-auto max-w-md" src="https://img.pokemondb.net/artwork/large/bulbasaur.jpg">
-    </div>
-  
-    <div class="w-1/3 h-12">
-      <div class="overflow-x-auto pt-6 pr-32">
-    <table class="table">
-      <!-- head -->
-      <thead>
-        <tr class="text-right text-xl">
-          <th>Pokedex Data</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <!-- row 1 -->
-        <tr class="hover">
-          <td class = "text-right">National №</td>
-          <td>0001</td>
-        </tr>
-        <!-- row 2 -->
-        <tr class="hover">
-          <td class = "text-right">Type</td>
-          <td>Grass</td>
-        </tr>
-        <!-- row 3 -->
-        <tr class="hover">
-          <td class = "text-right">Species</td>
-          <td>Seed Pokemon</td>
-        </tr>
-        <!-- row 4 -->
-        <tr class="hover">
-          <td class = "text-right">Height</td>
-          <td>0.7 m (2′04″)</td>
-        </tr>
-        <!-- row 5 -->
-        <tr class="hover">
-          <td class = "text-right">Weight</td>
-          <td>6.9 kg (15.2 lbs)</td>
-        </tr>
-        <!-- row 6 -->
-        <tr class="hover">
-          <td class = "text-right">Abilities</td>
-          <td>1. Overgrow<p></p>
-Chlorophyll (hidden ability)</td>
-        </tr>
-        <!-- row 7 -->
-        <tr class="hover">
-          <td class = "text-right">Local №</td>
-          <td>0001 (Red/Blue/Yellow)
-0226 (Gold/Silver/Crystal)<p></p>
-0001 (FireRed/LeafGreen)<p></p>
-0231 (HeartGold/SoulSilver)<p></p>
-0080 (X/Y — Central Kalos)<p></p>
-0001 (Let's Go Pikachu/Let's Go Eevee)<p></p>
-0068 (The Isle of Armor)</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-    </div>
-  
-    <div class="w-1/3 h-12">
-      <div class="overflow-x-auto pt-6 pr-32">
-    <table class="table">
-      <!-- head -->
-      <thead>
-        <tr class="text-right text-xl">
-          <th>Training</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <!-- row 1 -->
-        <tr class="hover">
-          <td class = "text-right">EV Yield</td>
-          <td>1 Sp. Atk</td>
-        </tr>
-        <!-- row 2 -->
-        <tr class="hover">
-          <td class = "text-right">Catch Rate</td>
-          <td>45 (5.9% with PokéBall, full HP)</td>
-        </tr>
-        <!-- row 3 -->
-        <tr class="hover">
-          <td class = "text-right">Base Friendship</td>
-          <td>50 (normal)</td>
-        </tr>
-        <!-- row 4 -->
-        <tr class="hover">
-          <td class = "text-right">Base Exp.</td>
-          <td>64</td>
-        </tr>
-        <!-- row 5 -->
-        <tr class="hover">
-          <td class = "text-right">Growth Rate</td>
-          <td>Medium Slow</td>
-        </tr>
-      </tbody>
-    </table>
-    <div class="overflow-x-auto pt-6">
-    <table class="table">
-      <!-- head -->
-      <thead>
-        <tr class="text-right text-xl">
-          <th>Breeding</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <!-- row 1 -->
-        <tr class="hover">
-          <td class = "text-right">Egg Groups</td>
-          <td>Grass, Monster</td>
-        </tr>
-        <!-- row 2 -->
-        <tr class="hover">
-          <td class = "text-right">Gender</td>
-          <td>87.5% male, 12.5% female</td>
-        </tr>
-        <!-- row 3 -->
-        <tr class="hover">
-          <td class = "text-right">Egg Cycles</td>
-          <td>20 (4,884–5,140 steps)</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-  </div>
-    </div>
+	<div class="grid place-items-center grid-cols-3 flex-wrap">
+		<div>
+			<img
+				class="h-390 w-363"
+				src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" />
+		</div>
 
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-    
-  </div>
-  
-  </template>
+		<div>
+			<table class="table">
+				<tbody>
+					<tr>
+						<th>Pokedex Number</th>
+						<td>001</td>
+					</tr>
+					<tr>
+						<th>Type</th>
+						<td>Grass, Poison</td>
+					</tr>
+					<tr>
+						<th>Height</th>
+						<td>0.7 m (2′04″)</td>
+					</tr>
+					<tr>
+						<th>Weight</th>
+						<td>6.9 kg (15.2 lbs))</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<div class="baseStats">
+			<h2 class="PokemonStatTitle">Base Stats</h2>
+			<p class="PokemonStat"><span class="statName">HP:</span> 45</p>
+			<p class="PokemonStat"><span class="statName">Attack:</span> 49</p>
+			<p class="PokemonStat"><span class="statName">Defense:</span> 49</p>
+			<p class="PokemonStat"><span class="statName">Sp. Atk:</span> 65</p>
+			<p class="PokemonStat"><span class="statName">Sp. Def:</span> 65</p>
+			<p class="PokemonStat"><span class="statName">Speed:</span> 45</p>
+			<div class="generalStats">
+				<h2 class="PokemonStatTitle">General Stats</h2>
+				<p class="PokemonStat">
+					<span class="statName">Weight (kg):</span> 6.9
+				</p>
+				<p class="PokemonStat"><span class="statName">Height (m):</span> 0.7</p>
+				<p class="PokemonStat"><span class="statName">Generation:</span> 1</p>
+				<p class="PokemonStat">
+					<span class="statName">Gender Ratio:</span>
+					<span class="Male">88 </span><span class="Female">12</span>
+				</p>
+				<p class="PokemonStat">
+					<span class="statName">Abilities:</span> Overgrow, Chlorophyll
+				</p>
+			</div>
+		</div>
+		<div grid grid-rows-2 flex-wrap>
+			<div class="flex items-center justify-center">Strong Against</div>
+			<div class="grid flex-wrap grid-cols-4 place-items-center">
+				<h3 class="fire">FIRE</h3>
+				<h3 class="flying">FLYING</h3>
+				<h3 class="ice">ICE</h3>
+				<h3 class="psychic">PSYCHIC</h3>
+			</div>
+			<div class="flex items-center justify-center">Weak Against</div>
+			<div class="grid flex-wrap grid-cols-4 place-items-center">
+				<div class="electric">ELECTRIC</div>
+				<div class="fairy">FAIRY</div>
+				<div class="fighting">FIGHTING</div>
+				<div class="grass">GRASS</div>
+				<div class="water">WATER</div>
+			</div>
+		</div>
+	</div>
+</template>
