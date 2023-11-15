81b3ebffa57104cd01e1c918ef8a788c764f9229
Reuben D'Souza <reubenjds@gmail.com>
2023-11-15 16:14:15 -0500

    fix: merge conflict

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,16 @@
+/** @format */
+
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (e) => console.error(e));
+db.once("open", () => console.log("Connected to server"));
 
 const app = express();
 app.use(cors({ origin: true }));
