60a7aed34ba6912ab6a006b2a6328dd9228bef35
Reuben D'Souza <reubenjds@gmail.com>
2023-11-16 14:47:38 -0500

    feat: add PokeBar component

---
+++ components/PokeBar.vue
@@ -0,0 +1,19 @@
+<!-- @format -->
+
+<template>
+	<div class="btn btn-block btn-circle flex">
+		<img
+			src="https://img.pokemondb.net/sprites/sword-shield/icon/ivysaur.png" />
+		<div class="font-bold text-white">{{ pokemon }}</div>
+		<div class="flex flex-col gap-2">
+			<div class="grass px-2">GRASS</div>
+			<div class="poison px-2">POISON</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ["pokemon"],
+	};
+</script>
---
+++ pages/pokemon.vue
@@ -0,0 +1,8 @@
+<!-- @format -->
+
+<template>
+	<div class="flex flex-wrap items-center justify-center h-screen">
+		<PokeBar pokemon="Bulbasaur"></PokeBar>
+		<div class="flex flex-col items-center justify-center"></div>
+	</div>
+</template>
