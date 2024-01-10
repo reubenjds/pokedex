823f20aba3c2d42848137870e6bc273b865d11a5
Reuben D'Souza <reubenjds@gmail.com>
2024-01-10 18:17:41 -0500

    fix: typo

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 <template>
-    <div class="hero min-h-screen" style="background-image: url(https://wallpapercave.com/wp/wp3170170.jpg);">
-        <div class="hero-overlay bg-opacity-60"></div>
-        <div class="hero-content text-center text-white">
-            <div class="max-width-lg">
-                <h1 class="mb-5 text-5xl font-bold">Welcome to Pokédex</h1>
-                <NuxtLink to="/pokemon"><button class="btn btn-error">Get Started</button></NuxtLink>
-            </div>
-        </div>
-    </div>
+	<div
+		class="hero min-h-screen"
+		style="background-image: url(https://wallpapercave.com/wp/wp3170170.jpg)">
+		<div class="hero-overlay bg-opacity-60"></div>
+		<div class="hero-content text-center text-white">
+			<div class="max-width-lg">
+				<h1 class="mb-5 text-5xl font-bold">Welcome to Pokédex</h1>
+				<NuxtLink to="/pokemon"
+					><button class="btn btn-error">Explore!</button></NuxtLink
+				>
+			</div>
+		</div>
+	</div>
 </template>
