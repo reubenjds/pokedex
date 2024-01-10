1f2ce9a58406095ff38b0dc1bdf691b36c669139
Reuben D'Souza <reubenjds@gmail.com>
2024-01-09 22:19:18 -0500

    fix: sprite image sizing

--- components/PokeBar.vue
+++ components/PokeBar.vue
@@ -1,6 +1,9 @@
 <template>
-	<NuxtLink :href="'/pokemon/' + pokedexNumber" class="bg-base-300 p-4 rounded-2xl flex flex-row w-full">
-		<img :src="spriteSmall" />
+	<NuxtLink :href="'/pokemon/' + pokedexNumber"
+		class="bg-base-300 p-4 rounded-2xl flex flex-row w-full place-items-center">
+		<div class="h-[44px] w-[56px] bg-transparent">
+			<img class="w-full h-full object-cover" :src="spriteSmall" />
+		</div>
 		<div class="w-full">
 			<div class="font-bold text-white text-center">{{ name }}</div>
 			<div class="flex flex-row gap-2 justify-center">
